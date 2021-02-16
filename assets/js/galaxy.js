87035c594e89f892be196e2b01c62c744f16cc65
Aramantos <john.doyle.mail@icloud.com>
2021-02-16 19:53:52 +0000

    index.html - added delay for path to riddle.html

--- assets/js/galaxy.js
+++ assets/js/galaxy.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
     $("#galaxy-button").click(function () {
         $(".intro-para").addClass("intro-para-zoom-out")
     });
-})
 
+    $("#galaxy-button").click(function () {
+        setTimeout(function () {
+            window.location.pathname = "riddle.html"
+        }, 5000);
+        $("#galaxy-image").addClass("galaxy-zoom")
+    });
 
+});
