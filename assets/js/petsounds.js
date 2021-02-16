f94c1397320e72c563a958ef73e8f7ecad1f64c3
Aramantos <john.doyle.mail@icloud.com>
2021-02-16 21:20:06 +0000

    riddle.html - added functionality where pet sounds play when correct animal is inputted

---
+++ assets/js/petsounds.js
@@ -0,0 +1,36 @@
+String.prototype.crosscheck = function () {
+    return this.toUpperCase().trim();
+};
+
+$(document).ready(function () {
+
+    const answerArray = [["Norwegian", "Dane", "Brit", "German", "Swede"], ["yellow", "blue", "red", "green", "white"],
+    ["cats", "horses", "birds", "fish", "dogs"], ["Dunhill", "Blends", "Pall Mall", "Prince", "Bluemaster"],
+    ["water", "tea", "milk", "coffee", "beer"]];
+
+    var cats = new Audio('./assets/media/cat.mp4')
+    var horses = new Audio('./assets/media/horse.mp4')
+    var birds = new Audio('./assets/media/bird.mp4')
+    var dogs = new Audio('./assets/media/dog.mpeg')
+
+    $("#B3").on("change", function () {
+        if (answerArray[2][0].crosscheck() === $("#B3").val().crosscheck())
+            cats.play();
+    });
+
+    $("#C3").on("change", function () {
+        if (answerArray[2][1].crosscheck() === $("#C3").val().crosscheck())
+            horses.play();
+    });
+
+    $("#D3").on("change", function () {
+        if (answerArray[2][2].crosscheck() === $("#D3").val().crosscheck())
+            birds.play();
+    });
+
+    $("#F3").on("change", function () {
+        if (answerArray[2][4].crosscheck() === $("#F3").val().crosscheck())
+            dogs.play();
+    });
+
+});
