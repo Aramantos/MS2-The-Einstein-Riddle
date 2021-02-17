0ad035beab6d4e0ecf50095c08759a9dc11dfe53
Aramantos <john.doyle.mail@icloud.com>
2021-02-17 20:13:45 +0000

    index.html - readjusted riddle.html path to attempt to fix 404 error

--- assets/js/galaxy.js
+++ assets/js/galaxy.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
     $("#galaxy-button").click(function () {
         setTimeout(function () {
-            window.location.pathname = "/riddle.html"
+            window.location.href = window.location.href + "riddle.html"
         }, 5000);
         $("#galaxy-image").addClass("galaxy-zoom")
     });
